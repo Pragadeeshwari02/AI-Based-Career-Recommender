fe67018db8e837bf954e7e08c4a2d1969a3fc19b
Pragadeeshwari R R <pragadeeshwarirajkumar@gmail.com>
2026-01-04 16:30:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,url_for,render_template
+import os
 
 app=Flask(__name__)
 
@@ -73,4 +74,5 @@ def plot_graph(career):
     plt.close()
 
 if __name__=="__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
